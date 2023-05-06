0db457e5f20caa3c7dfd797b3202052f6a089732
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-05-07 00:36:25 +0300

    Modify app js for new components route

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Dishes from './components/Dishes';
+import Recipe from './components/Recipe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dishes />} />
+        <Route path="recipe" element={<Recipe />} />
+      </Route>
+    </Routes>
   );
 }
 
